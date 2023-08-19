e19ea88581e98b66015b2dc2639276292fcd7e32
John Christian Tan <jchristian.abante@gmail.com>
2023-08-19 13:50:40 +0800

    ST-007 - Add buttons

--- src/components/UserFile.jsx
+++ src/components/UserFile.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import ProfilePicture from './ProfilePicture'
+import React from "react";
+import ProfilePicture from "./ProfilePicture";
+import Buttons from "./Buttons";
 
 const UserFile = () => {
-    return (
-        <div>
-            <ProfilePicture />
-        </div>
-    )
-}
+  return (
+    <div>
+      <ProfilePicture />
+      <Buttons />
+    </div>
+  );
+};
 
-export default UserFile
+export default UserFile;
