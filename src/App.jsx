7ab1098f6804b059b6b3e5d0d5407cc1ede57418
ely <eleazar.odivilas01@gmail.com>
2023-08-19 14:38:56 +0800

    ST-008 - Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from "react";
 import "./App.css";
 import ProfilePicture from "./components/ProfilePicture";
 import UserFile from "./components/UserFile";
 import Footer from "./components/Footer";
 
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <UserFile />
-
       <Footer />
     </div>
   );
