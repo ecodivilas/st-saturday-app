24d4bc609e4ec9d5db8f4d8afdc8e699e21c5b7b
Melven <melvenbagolaque@gmail.com>
2023-08-19 12:16:20 +0800

    ST-009 Added footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/icons/Icons.jsx
@@ -0,0 +1,6 @@
+import { BiLogoFacebook } from 'react-icons/bi'
+import { BsTwitter } from 'react-icons/bs'
+import { AiFillInstagram } from 'react-icons/ai'
+
+
+export { BiLogoFacebook, BsTwitter , AiFillInstagram}
